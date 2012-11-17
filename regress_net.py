c5c1ca8da1cf31578e31be1ddea55d22dd99f70d
Nicholas Eggert <nic.eggert@gmail.com>
2012-11-17 14:08:25 -0500

    Updates to regress_net

--- regress_net.py
+++ regress_net.py
@@ -18,7 +18,7 @@ class RegressConvNet(object):
         self.l1_penalty = l1_penalty
 
         self.conv_layer = layers.ConvLayer(input, rng, image_shape=input_shape, filter_shape=filter_shape)
-        self.hidden_layer = layers.LogisticLayer(self.conv_layer.output, rng, filter_shape[1]*(input_shape[3]-filter_shape[3]+1)**2, n_hidden)
+        self.hidden_layer = layers.LogisticLayer(self.conv_layer.output, rng, filter_shape[0]*(input_shape[3]-filter_shape[3]+1)**2, n_hidden)
         self.output_layer = layers.RectifiedLinearLayer(self.hidden_layer.output, rng, n_hidden, n_out)
 
         self.output = self.output_layer.output
@@ -39,7 +39,7 @@ class RegressConvNet(object):
                              self.output_layer.b.get_value()))
 
     def load_params(self, filename):
-        cW, cb, hW, hb, oW, ob = np.load(filename)
+        cW, hW, hb, oW, ob = np.load(filename)
         self.conv_layer.W.set_value(cW)
         self.hidden_layer.W.set_value(hW)
         self.hidden_layer.b.set_value(hb)
@@ -87,18 +87,18 @@ class RegressNetWithDropoutPredict(RegressConvNet):
 
 
 
-def train_regress_net(train_size=200, valid_size=60, iterations=1000, momentum_decay=0.9, learning_rate=0.7, filter_size=10, n_hidden=500, plot=False):
-    # theano.config.compute_test_value = 'warn'
+def train_regress_net(train_size=200, valid_size=60, iterations=10000, momentum_decay=0.9, learning_rate=0.7, filter_size=10, n_hidden=500, n_filters=6, plot=False):
+    theano.config.compute_test_value = 'off'
     theano.config.DebugMode.check_strides = 0
 
 
     # initialize some stuff
     nbins_out = 6
-    batch_size=2*train_size
+    batch_size=8*train_size
     rng = np.random.RandomState(4321)
 
     # load the data
-    datasets = utilities.load_data("data/train_skies_grid.npz", train_size, valid_size, flip=True, rotate=False)
+    datasets = utilities.load_data("data/train_skies_grid.npz", train_size, valid_size, flip=True, rotate=True)
     train_set_x, train_set_y = datasets[0]
     valid_set_x, valid_set_y = datasets[1]
     test_set_x, test_set_y = datasets[2]
@@ -113,7 +113,7 @@ def train_regress_net(train_size=200, valid_size=60, iterations=1000, momentum_d
     # target.tag.test_value = train_set_y.get_value()
 
     # create the net
-    net_params = [rng, [batch_size, 3, nbins,nbins], (6, 3, filter_size,filter_size), n_hidden, nbins_out, .001]
+    net_params = [rng, [batch_size, 3, nbins,nbins], (n_filters, 3, filter_size,filter_size), n_hidden, nbins_out, .001]
     cls = RegressNetWithDropoutTrain(data, *net_params )
 
     # create a validation net
@@ -123,7 +123,7 @@ def train_regress_net(train_size=200, valid_size=60, iterations=1000, momentum_d
 
     #Sanity check to make sure the net works
     cost = theano.function(inputs=[],
-                           outputs=cls.costL1(target),
+                           outputs=cls.cost(target),
                            givens={data:train_set_x,
                                    target: train_set_y})
 
@@ -134,24 +134,27 @@ def train_regress_net(train_size=200, valid_size=60, iterations=1000, momentum_d
     # Currently using gradient decent with momentum
     grads = T.grad(cls.cost(target), cls.params)
 
+    lr = T.scalar('lr')
+
     updates = {}
     momentum = {}
-    for p, g in zip(cls.params, grads):
-        momentum[p] = theano.shared(np.zeros_like(p.get_value(), dtype=theano.config.floatX))
-        updates[p] = p+learning_rate*(momentum_decay*momentum[p]-(1-momentum_decay)*g)
-        updates[momentum[p]] = learning_rate*(momentum_decay*momentum[p]-(1-momentum_decay)*g)
+    learning_rate_scales = [1., 1., 1., 1.]
+    for p, g, ls in zip(cls.params, grads, learning_rate_scales):
+        momentum[p] = theano.shared(np.zeros_like(p.get_value()))
+        updates[p] = p+ls*lr*(momentum_decay*momentum[p]-(1-momentum_decay)*g)
+        updates[momentum[p]] = momentum_decay*momentum[p]-(1-momentum_decay)*g
 
     # compile the training function in theano
-    train_model_debug = theano.function(inputs=[],
-                                  outputs=[cls.cost(target), cls.output, cls.conv_layer.output, cls.hidden_layer.output],
-                                  givens = {
-                                    data: train_set_x,
-                                    target: train_set_y
-                                  },
-                                  updates = updates
-                                  # ,mode="DebugMode"
-                                 )
-    train_model = theano.function(inputs=[],
+    # train_model_debug = theano.function(inputs=[],
+    #                               outputs=[cls.cost(target), cls.output, cls.conv_layer.output, cls.hidden_layer.output],
+    #                               givens = {
+    #                                 data: train_set_x,
+    #                                 target: train_set_y
+    #                               },
+    #                               updates = updates
+    #                               # ,mode="DebugMode"
+    #                              )
+    train_model = theano.function(inputs=[lr],
                                   outputs=cls.cost(target),
                                   givens = {
                                     data: train_set_x,
@@ -184,14 +187,35 @@ def train_regress_net(train_size=200, valid_size=60, iterations=1000, momentum_d
             vc = validation_cost()
             print "Validation Cost:", vc
             val_score.append(vc)
-            # print "Validation Prediction\n", validation_pred()
-            tc = train_model()
+            print "Validation Prediction\n", validation_pred()[-1]
+            print "Acutal value: ", valid_set_y.get_value()[-1]
+            # print "Linear weights"
+            # print cls.params[-2].get_value()
+            tc = train_model(max([learning_rate*i/1000., learning_rate]))
             print tc
             train_score.append(tc)
-        train_model()
+            if i > 1500:
+                # check stopping condition
+                # linear least squares to last 10 points in train_score
+                # see np.linalg.lstsq for explanation of how this works
+                A = np.vstack([np.arange(10)*100, np.ones(10)]).T
+                y = np.asarray(train_score[-10:])
+                slope, intercept = np.linalg.lstsq(A, y)[0]
+                if abs(slope) < 1:
+                    print "{} iterations".format(i)
+                    print "Final slope: ", slope
+                    print "Final intercept: ", intercept
+                    break
+        train_model(max([learning_rate*i/1000., learning_rate]))
 
         # import pdb
         # pdb.set_trace()
+
+    print "Final Training Cost: {}".format(train_model(0.))
+    print "Final Validation Cost: {}".format(validation_cost())
+
+    print "Validation preditions"
+    print validation_pred()
 
     # save the model parameters
     cls.save_params("test_weights_regress.npy")
@@ -202,3 +226,27 @@ def train_regress_net(train_size=200, valid_size=60, iterations=1000, momentum_d
         plt.plot(train_score)
         plt.legend(["Validation Cost", "Training Cost"])
         plt.show()
+
+
+def predict(filter_size=10, n_hidden=500, n_filters=6):
+    data_vals, sky_names = utilities.load_test("data/test_skies_grid.npz")
+
+    batch_size=data_vals.shape[0]
+    nbins=42
+    nbins_out = 6
+
+    # prepare theano objects
+    data = T.tensor4('x')
+    # data.tag.test_value = train_set_x.get_value()
+
+    # create the net
+    rng = np.random.RandomState(4321)
+    net_params = [rng, [batch_size, 3, nbins,nbins], (n_filters, 3, filter_size,filter_size), n_hidden, nbins_out, .001]
+    cls = RegressConvNet(data, *net_params )
+    cls.load_params("test_weights_regress.npy")
+
+    predict = theano.function([data], cls.output)
+
+    predictions = predict(data_vals)
+
+    print predictions
